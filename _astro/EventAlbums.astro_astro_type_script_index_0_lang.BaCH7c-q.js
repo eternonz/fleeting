1eb007900cf4a1922224d7a1f409951f6649fc6e
eterno <timestorynz@gmail.com>
2026-06-02 00:22:09 +1200

    Deploy site-v2 build 20260601-122209

--- _astro/EventAlbums.astro_astro_type_script_index_0_lang.BaCH7c-q.js
+++
@@ -1,1 +0,0 @@
-import{P as c}from"./site.CpJhQJ91.js";for(const t of document.querySelectorAll("[data-gated-card]")){const o=t.dataset.ev;o&&fetch(`${c}/api/gift?ev=${encodeURIComponent(o)}`).then(e=>e.json()).then(e=>{e.status!=="locked"&&t.remove()}).catch(()=>{})}
